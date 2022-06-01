81f6390f81eb37b2ed3e1781f68ebc88d14df447
Strong10mede <kmayur809@gmail.com>
2022-06-01 13:03:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [likes, setLikes] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Total number of likes {likes} </h1>
+        <button onClick={() => setLikes(likes + 1)}>Like my repos</button>
+        <p>try to smash that like options on my repos </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Strong10mede/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My GitHub Account
         </a>
       </header>
     </div>
